89068c41d919bcbbb1d9fded13c4190c1e3dce74
S SANTHOSH KUMAR <88322124+kumar6268@users.noreply.github.com>
2024-09-21 09:12:53 +0000

    dataType

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountID = 123 // const data type can not be asssignment to constant variable
+// accountID = 2
+
+
+var accountEmail = "abcd@gmail.com" /* prefer not to use var
+becuase of issue in block scope and functinal scop
+                                    */ 
+
+console.table([accountID, accountEmail]);
+
---
+++ 01_Basic/02-dataType.js
@@ -0,0 +1,16 @@
+// "use strict"; // treat all JS code as newe version
+
+// alert(3 + e);
+let name = "bacn" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+let = null // standalone value
+let = undefined // no value given
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
