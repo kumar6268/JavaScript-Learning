af40bbf532e36f65baf21e0a94d51fe640aa80bc
S SANTHOSH KUMAR <88322124+kumar6268@users.noreply.github.com>
2024-09-23 02:44:24 +0000

    if else topic

---
+++ 04_AllcontrolFlowsTopics/01_if-else.js
@@ -0,0 +1,25 @@
+// if,else condition
+// <, >, <=, >=, ==, ===, !== comparison
+
+// const temparature = 30;
+
+// if (temparature !== 31){
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+//}
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard === false) {
+    console.log("yes");
+} else if (userLoggedIn && debitCard === false) {
+    console.log("No"); 
+} else{
+    console.log("optinal");
+    
+}
+
+
+
