fa93b1b01957a5be7064bf3177388525a7e32290
S SANTHOSH KUMAR <88322124+kumar6268@users.noreply.github.com>
2024-09-23 02:44:52 +0000

    switch statment topic

---
+++ 04_AllcontrolFlowsTopics/02_switch.js
@@ -0,0 +1,19 @@
+// switch constrol flow
+
+const month = 4;
+
+switch (month) {
+    case 2:
+        console.log("Fed");
+        break;
+    case 3:
+    console.log("March");
+    break
+case 4:
+    console.log("May");
+    break
+
+    default:
+        console.log("default value print");
+        break;
+}
