7db08a0ebc53f2d0641fc1f231f55b1082f32bfa
S SANTHOSH KUMAR <88322124+kumar6268@users.noreply.github.com>
2024-09-28 09:57:04 +0000

    synANDasync topic

---
+++ 08_eventBasic/02_syn&async.js
@@ -0,0 +1,20 @@
+/*
+JAVASCRIPT
+
+1) JAVASCRIPT Synchronous and it a single threaded execution process
+
+-> Execution context
+1) Execute one line of code at a time
+
+
+Blocking code V/s Non Blocking Code
+1) Blocking code
+Block the flow of program
+Read file sync
+
+2) Non Blocking Code
+
+Does not block the execution
+Read file async
+
+*/
